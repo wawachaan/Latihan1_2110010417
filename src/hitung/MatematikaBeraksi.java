3665f16000d48815d02e88eebaf1d90602c06718
wawachaan <najwanorizat4@gmail.com>
2023-06-12 16:11:09 +0700

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -3,5 +3,13 @@ package hitung;
 
 
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+    //membuat objek
+    Matematika najwa = new Matematika (1,7);
     
+    System.out.println("Hasil penjumlahan : "+najwa.setPenjumlahan());
+    System.out.println("Hasil pengurangan : "+najwa.setPengurangan());
+    System.out.println("Hasil perkalian   : "+najwa.setPerkalian());
+    System.out.println("Hasil pembagian   : "+najwa.setPembagian());
+    } 
 }
